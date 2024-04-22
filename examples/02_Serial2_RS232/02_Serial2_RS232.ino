f47a79b5c4d804ed923da09926c59e39f382334c
dong-higenis <dong@higenis.com>
2024-04-22 15:55:51 +0900

    build 파일 생성

--- examples/02_Serial2_RS232/02_Serial2_RS232.ino
+++ examples/02_Serial2_RS232/02_Serial2_RS232.ino
@@ -2,7 +2,7 @@ uint8_t rs232TxPin = 4;
 uint8_t rs232RxPin = 5;
 void setup() {
   Serial.begin(115200);
-  Serial3.begin(115200, SERIAL_8N1, rs232RxPin, rs232TxPin);
+  Serial2.begin(115200, SERIAL_8N1, rs232RxPin, rs232TxPin);
 
   Serial.println("hello RS232");
 }
